d074e8158e8af78b9940ac601c451a4bd6fe43f1
Igor Fedorenko <igor@ifedorenko.com>
2012-01-07 17:26:23 -0500

    More robust javaagent implementation
    Check protectionDomain and all intermediate values for nulls.
    
    Catch and print all exceptions to collect futher debug
    information.
    
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- com.ifedorenko.m2e.sourcelookup.javaagent/src/main/java/com/ifedorenko/m2e/sourcelookup/javaagent/Premain.java
+++ com.ifedorenko.m2e.sourcelookup.javaagent/src/main/java/com/ifedorenko/m2e/sourcelookup/javaagent/Premain.java
@@ -13,6 +13,8 @@ package com.ifedorenko.m2e.sourcelookup.javaagent;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.net.URL;
+import java.security.CodeSource;
 import java.security.ProtectionDomain;
 
 import org.objectweb.asm.ClassReader;
@@ -32,40 +34,65 @@ public class Premain
                                      ProtectionDomain protectionDomain, byte[] classfileBuffer )
                 throws IllegalClassFormatException
             {
-                final String location = protectionDomain.getCodeSource().getLocation().toExternalForm();
-
-                final ClassReader r = new ClassReader( classfileBuffer, 0, classfileBuffer.length );
-                final ClassWriter w = new ClassWriter( 0 );
-
-                r.accept( new ClassVisitor( Opcodes.ASM4, w )
+                try
                 {
-                    public void visitSource( String source, String debug )
+                    if ( protectionDomain == null )
                     {
-                        if ( debug != null )
+                        return null;
+                    }
+
+                    final CodeSource codeSource = protectionDomain.getCodeSource();
+                    if ( codeSource == null )
+                    {
+                        return null;
+                    }
+
+                    final URL locationUrl = codeSource.getLocation();
+                    if ( locationUrl == null )
+                    {
+                        return null;
+                    }
+
+                    final String location = locationUrl.toExternalForm();
+
+                    final ClassReader r = new ClassReader( classfileBuffer, 0, classfileBuffer.length );
+                    final ClassWriter w = new ClassWriter( 0 );
+
+                    r.accept( new ClassVisitor( Opcodes.ASM4, w )
+                    {
+                        public void visitSource( String source, String debug )
                         {
-                            System.err.println( "m2e SMAP merge is not supported!" );
-                            System.err.println( debug );
-                        }
-                        else
-                        {
-                            StringBuilder smap = new StringBuilder();
-                            smap.append( "SMAP\n" );
-                            smap.append( source ).append( "\n" );
-                            smap.append( "Java\n" );
-                            smap.append( "*S m2e\n" );
-                            smap.append( "*F\n" );
-                            smap.append( "1 " ).append( location ).append( "\n" );
-                            smap.append( "*L\n" );// JDT insists on LineSection
-                            smap.append( "*E\n" );
-                            debug = smap.toString();
-                        }
+                            if ( debug != null )
+                            {
+                                System.err.println( "m2e SMAP merge is not supported!" );
+                                System.err.println( debug );
+                            }
+                            else
+                            {
+                                StringBuilder smap = new StringBuilder();
+                                smap.append( "SMAP\n" );
+                                smap.append( source ).append( "\n" );
+                                smap.append( "Java\n" );
+                                smap.append( "*S m2e\n" );
+                                smap.append( "*F\n" );
+                                smap.append( "1 " ).append( location ).append( "\n" );
+                                smap.append( "*L\n" );// JDT insists on LineSection
+                                smap.append( "*E\n" );
+                                debug = smap.toString();
+                            }
 
-                        super.visitSource( source, debug );
-                    };
+                            super.visitSource( source, debug );
+                        };
 
-                }, 0 );
+                    }, 0 );
 
-                return w.toByteArray();
+                    return w.toByteArray();
+                }
+                catch ( Exception e )
+                {
+                    e.printStackTrace();
+                }
+                return null;
             }
         } );
     }

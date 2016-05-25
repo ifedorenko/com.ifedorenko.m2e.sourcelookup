1dce63fbc444d60aeb4283f2847d6765f4402569
Igor Fedorenko <igor@ifedorenko.com>
2016-05-24 22:12:49 -0400

    fixed number of issues tracking and matching workspace projects
    * didn't correctly listen for all relevant java model events
    * didn't match workspace projects by hash of their output
      locations (useful for binary projects)
    * didn't correctly synchronize access to hash->project multimap
      and project info
    
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- com.ifedorenko.m2e.sourcelookup/src/com/ifedorenko/m2e/sourcelookup/internal/jdt/WorkspaceProjects.java
+++ com.ifedorenko.m2e.sourcelookup/src/com/ifedorenko/m2e/sourcelookup/internal/jdt/WorkspaceProjects.java
@@ -10,6 +10,11 @@
  *******************************************************************************/
 package com.ifedorenko.m2e.sourcelookup.internal.jdt;
 
+import static org.eclipse.jdt.core.IJavaElementDelta.F_CLASSPATH_CHANGED;
+import static org.eclipse.jdt.core.IJavaElementDelta.F_CLOSED;
+import static org.eclipse.jdt.core.IJavaElementDelta.F_OPENED;
+import static org.eclipse.jdt.core.IJavaElementDelta.F_RESOLVED_CLASSPATH_CHANGED;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -38,6 +43,9 @@ import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.launching.sourcelookup.containers.PackageFragmentRootSourceContainer;
 
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multimap;
 import com.ifedorenko.m2e.sourcelookup.internal.jdt.IProjectSourceDescriber.IJavaProjectSourceDescription;
 import com.ifedorenko.m2e.sourcelookup.internal.jdt.IProjectSourceDescriber.ISourceContainerFactory;
@@ -82,13 +90,13 @@ public class WorkspaceProjects {
               add.add((IJavaProject) element);
               break;
             case IJavaElementDelta.CHANGED:
-              switch (delta.getFlags()) {
-                case IJavaElementDelta.F_CLOSED:
-                  remove.add((IJavaProject) element);
-                  break;
-                case IJavaElementDelta.F_OPENED:
-                  add.add((IJavaProject) element);
-                  break;
+              if ((delta.getFlags() & F_CLOSED) != 0) {
+                remove.add((IJavaProject) element);
+              } else if ((delta.getFlags() & F_OPENED) != 0) {
+                add.add((IJavaProject) element);
+              } else if ((delta.getFlags() & (F_CLASSPATH_CHANGED | F_RESOLVED_CLASSPATH_CHANGED)) != 0) {
+                remove.add((IJavaProject) element);
+                add.add((IJavaProject) element);
               }
               break;
           }
@@ -110,15 +118,31 @@ public class WorkspaceProjects {
   };
 
   private static class JavaProjectDescription implements IJavaProjectSourceDescription {
-    final Set<File> locations = new HashSet<>();
+    final Set<File> locations;
 
-    final Set<Object> hashes = new HashSet<>();
+    final Set<Object> hashes;
 
-    final List<ISourceContainerFactory> factories = new ArrayList<>();
+    final List<ISourceContainerFactory> factories;
 
-    final Map<File, IPackageFragmentRoot> dependencyLocations = new HashMap<>();
+    final Map<File, IPackageFragmentRoot> dependencyLocations;
 
-    final Map<Object, IPackageFragmentRoot> dependencyHashes = new HashMap<>();
+    final Map<Object, IPackageFragmentRoot> dependencyHashes;
+
+    public JavaProjectDescription() {
+      locations = new HashSet<>();
+      hashes = new HashSet<>();
+      factories = new ArrayList<>();
+      dependencyLocations = new HashMap<>();
+      dependencyHashes = new HashMap<>();
+    }
+
+    private JavaProjectDescription(JavaProjectDescription original) {
+      locations = ImmutableSet.copyOf(original.locations);
+      hashes = ImmutableSet.copyOf(original.hashes);
+      factories = ImmutableList.copyOf(original.factories);
+      dependencyLocations = ImmutableMap.copyOf(original.dependencyLocations);
+      dependencyHashes = ImmutableMap.copyOf(original.dependencyHashes);
+    }
 
     @Override
     public void addLocation(File location) {
@@ -146,6 +170,10 @@ public class WorkspaceProjects {
     public void addDependencies(Map<File, IPackageFragmentRoot> dependencies) {
       this.dependencyLocations.putAll(dependencies);
       this.dependencyHashes.putAll(Locations.hash(dependencies));
+    }
+
+    public JavaProjectDescription immutableCopy() {
+      return new JavaProjectDescription(this);
     }
   }
 
@@ -176,6 +204,14 @@ public class WorkspaceProjects {
     JavaProjectDescription description = getProjectByLocation(projectLocation);
 
     if (description == null) {
+      Collection<JavaProjectDescription> desciptions = getProjectsByHash(projectLocation);
+      if (!desciptions.isEmpty()) {
+        // it is totally possible to have multiple binary projects for the same jar
+        description = desciptions.iterator().next();
+      }
+    }
+
+    if (description == null) {
       return null;
     }
 
@@ -193,13 +229,19 @@ public class WorkspaceProjects {
     }
   }
 
+  private Collection<JavaProjectDescription> getProjectsByHash(File projectLocation) {
+    synchronized (lock) {
+      return ImmutableList.copyOf(hashes.get(Locations.hash(projectLocation)));
+    }
+  }
+
   public ISourceContainer getClasspathEntryContainer(File projectLocation, File entryLocation) {
     JavaProjectDescription projectByLocation = getProjectByLocation(projectLocation);
 
     IPackageFragmentRoot dependency = getProjectDependency(projectByLocation, entryLocation);
 
     if (dependency == null && projectByLocation == null) {
-      for (JavaProjectDescription projectByHash : hashes.get(Locations.hash(projectLocation))) {
+      for (JavaProjectDescription projectByHash : getProjectsByHash(projectLocation)) {
         dependency = getProjectDependency(projectByHash, entryLocation);
         if (dependency != null) {
           break;
@@ -254,6 +296,8 @@ public class WorkspaceProjects {
     for (IProjectSourceDescriber describer : getJavaProjectDescribers()) {
       describer.describeProject(project, info);
     }
+
+    info = info.immutableCopy();
 
     synchronized (lock) {
       for (File location : info.locations) {
